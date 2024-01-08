21c28c6fffc8850ccc16ee2be568ba1605ffa42e
Anton <anton22582258@gmail.com>
2024-01-08 16:56:09 +0300

    EmployeeSpringHomework

---
+++ src/main/java/pro/sky/employeespringhomework/EmployeeSpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeespringhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSpringHomeworkApplication.class, args);
+	}
+
+}
